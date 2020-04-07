9ccf71bf38f7b200b29283019d85c1c3edf29561
Hassan Taj <leehaisen01@gmail.com>
2020-04-07 22:06:56 +0500

    update babes

--- js/datafucker.js
+++ js/datafucker.js
@@ -1,17 +1,17 @@
 $(function () {
 
     const consts = {
-        skills : "skills",
-        languages : "languages",
+        skills: "skills",
+        languages: "languages",
         experiences: "experiences",
-        education : "education",
+        education: "education",
     };
 
     display = ".display-js";
     personaldetails = ".person-details-js";
     skills = ".skills-js";
     languages = ".languages-js";
-    exps =".experiences-js";
+    exps = ".experiences-js";
     education = ".education-js"
     about = ".about-js";
     pagetitle = "#page-title-js";
@@ -25,82 +25,102 @@ $(function () {
         console.log(profile);
 
         // sett profile
-        $(display).children('.profile-image-js').css("background-image",`url(${profile.image_url})`)
+        $(display).children('.profile-image-js').css("background-image", `url(${profile.image_url})`)
         $(pagetitle).text(profile.name)
-        $(fav).attr("href",profile.favicon)
+        $(fav).attr("href", profile.favicon)
         $(display).children().children('h2').text(profile.name)
         // set other details
         $(about).text(profile.about);
-        $(personaldetails).html(getPersonalDetailsTemplate(profile))
-        $(skills).html(RenderList(profile.skills,consts.skills));
-        $(languages).html(RenderList(profile.languages,consts.languages));
+        $(personaldetails).html(getPersonalDetailsTemplate(profile));
+        // skills
+        $(skills).html(RenderList(profile.skills, consts.skills));
+        $.each(profile.skills, function (i, e) {
+            var dial = $(`.dial_${e.id}`);
+            dial.knob({
+                'min': 10,
+                'max': 100,
+                'width': 85,
+                'height': 85,
+                'displayInput': true,
+                'fgColor': "#009688",
+            });
+            dial.css({"font-size":"12px"})
+            dial.val(`${e.name}`);
+            dial.parent().css({"margin":"0px 8px"})
+        });
+
+        // languages
+        $(languages).html(RenderList(profile.languages, consts.languages));
 
         // work exp
-        $(exps).html(RenderList(profile.experiences,consts.experiences))
+        $(exps).html(RenderList(profile.experiences, consts.experiences))
 
         // education 
-        $(education).html(RenderList(profile.education_history,consts.education))
+        $(education).html(RenderList(profile.education_history, consts.education))
     }).fail(function (a, b, error) {
 
     });
 
-
-    function getPersonalDetailsTemplate(p){
+    function getPersonalDetailsTemplate(p) {
         return `<p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>${p.designation}</p>
         <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>${p.address}</p>
         <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>${p.email}</p>
         <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>${p.phone}</p>`;
     }
 
-    function RenderList(list,template){
-        var res ='';
-        $.each(list,function(i,e){
-            if(template === consts.skills){
-                res += getSkillsTemplate(e);
+    function RenderList(list, template) {
+        var res = '';
+        $.each(list, function (i, e) {
+            if (template === consts.skills) {
+                res += getKnobTemplate(e);
             }
-            if(template === consts.languages){
+            if (template === consts.languages) {
                 res += getLanguagesTemplate(e);
             }
-            if(template === consts.experiences){
+            if (template === consts.experiences) {
                 res += getExperienceTemplate(e);
             }
-            if(template === consts.education){
+            if (template === consts.education) {
                 res += getEducationTemplate(e);
             }
         });
         return res;
     }
 
-    function getSkillsTemplate(e){
+    function getKnobTemplate(e) {
+        return `<input type="text" value="${e.score}" class="dial_${e.id}" data-linecap="round" disabled="disabled" readonly="readonly">`;
+    }
+
+    function getSkillsTemplate(e) {
         return `<p>${e.name}</p>
         <div class="w3-light-grey w3-round-xlarge w3-small">
             <div class="w3-container w3-center w3-round-xlarge w3-teal" style="width:${e.score}%">${e.score}%</div>
         </div>`;
     }
 
-    function getLanguagesTemplate(e){
+    function getLanguagesTemplate(e) {
         return `<p>${e.name}</p>
         <div class="w3-light-grey w3-round-xlarge">
             <div class="w3-round-xlarge w3-teal" style="height:24px;width:${e.score}%"></div>
         </div>`;
     }
 
-    function getExperienceTemplate(e){
-        var res =`<div class="w3-container">
+    function getExperienceTemplate(e) {
+        var res = `<div class="w3-container">
         <h5 class="w3-opacity"><b>${e.designation}</b></h5>
         <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right">
         </i>${e.from} - `;
-        res += e.isCurrent==true ? `<span class="w3-tag w3-teal w3-round">Current</span>`:`${e.to}`;
+        res += e.isCurrent == true ? `<span class="w3-tag w3-teal w3-round">Current</span>` : `${e.to}`;
         res += `</h6>`;
         res += `<p>${e.company}</p>
         <p>${e.description}</p>
         <hr>
         </div>`;
-        
+
         return res;
     }
 
-    function getEducationTemplate(e){
+    function getEducationTemplate(e) {
         return `<div class="w3-container">
         <h5 class="w3-opacity"><b>${e.school_name}</b></h5>
         <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>${e.from} - ${e.to}</h6>
